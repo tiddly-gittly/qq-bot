b6e49d58e225c5b328b3863b4ee02f5c703c2ac4
lin onetwo <linonetwo012@gmail.com>
2023-05-18 20:48:27 +0800

    fix: let pm2 works in win11
    https://github.com/Unitech/pm2/issues/5608

--- bot-wiki.config.js
+++ bot-wiki.config.js
@@ -16,17 +16,24 @@ module.exports = {
   apps: [
     {
       name: 'qq-bot',
-      script: 'cd ~/qq-bot && pnpm start',
+      // script: 'C:\\Program Files\\nodejs\\node_modules\\npm\\bin\\npm-cli.js',
+      // instances: 1,
+      // exec_mode: 'cluster',
+      script: 'npm',
+      args: 'run start',
+      cwd: 'D:/repo/qq-bot',
+      interpreter: process.platform === 'win32' && 'C:\\Windows\\System32\\cmd.exe',
     },
-    ...wikiDirs.flatMap(({ name, wikiDir }, index) => [
-      {
-        name,
-        script: `tiddlywiki ${wikiDir} --listen host=0.0.0.0 port=${8081 + index} root-tiddler=$:/core/save/lazy-images`,
-      },
-      {
-        name: `git-${name}`,
-        script: `cd ${wikiDir} && git pull`,
-      },
-    ]),
+    // on windows, this is started by TidGi
+    // ...wikiDirs.flatMap(({ name, wikiDir }, index) => [
+    //   {
+    //     name,
+    //     script: `tiddlywiki ${wikiDir} --listen host=0.0.0.0 port=${8081 + index} root-tiddler=$:/core/save/lazy-images`,
+    //   },
+    //   {
+    //     name: `git-${name}`,
+    //     script: `cd ${wikiDir} && git pull`,
+    //   },
+    // ]),
   ],
 };
