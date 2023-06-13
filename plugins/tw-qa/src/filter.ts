b93af4f8d974d45733e933e7b922d648f6cc5545
lin onetwo <linonetwo012@gmail.com>
2023-06-13 17:01:50 +0800

    Update filter.ts

--- plugins/tw-qa/src/filter.ts
+++ plugins/tw-qa/src/filter.ts
@@ -1,6 +1,6 @@
 // http://192.168.3.17:8081/recipes/default/tiddlers.json?filter=[!is[shadow]!is[system]field:type[text/vnd.tiddlywiki]susearch-sort:title,caption,text:raw-strip[git]first[10]]
 // from $:/plugins/EvidentlyCube/ExtraOperators
-const searchFilter = '[!is[shadow]!is[system]field:type[text/vnd.tiddlywiki]susearch-sort:title,text,caption:raw-strip[${query}]${filter}]';
+const searchFilter = '[!is[shadow]!is[system]field:type[text/vnd.tiddlywiki]susearch-sort:title,text,caption,keywords:raw-strip[${query}]${filter}]';
 export const buildWikiFilter = function (query, count = 5, pagination = 1) {
   // starts at page 1
   const paginationTimesCount = Math.max((pagination - 1) * count, 0);
